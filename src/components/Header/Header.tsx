ad454d037c623d611d1e71abc5f54901c3ec3265
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-30 16:19:07 +0100

    Add header component with Header.tsx file

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = (): React.ReactElement => {
+  return (
+    <header>
+      <img src="./public/images/logo.webp" alt="Logo of Better call Lau" />
+    </header>
+  );
+};
+
+export default Header;
