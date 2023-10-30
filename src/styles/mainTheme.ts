8c3f1e5336c97ca09346086d13160ee34eeefe7e
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-30 16:26:51 +0100

    Add main theme to proyect

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+import "@fontsource/playfair-display";
+import "@fontsource/karla";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    background: "#307e80",
+    primary: "#234442",
+    secondary: "#848d6b",
+    tertiary: "765846",
+    accent: "c1b585",
+  },
+  typography: {
+    main: "Playfair Display",
+    secondary: "Karla",
+  },
+};
+
+export default mainTheme;
