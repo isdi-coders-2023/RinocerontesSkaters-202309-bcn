de00326c3cb3cb4a8fe6ee120e6368b4bf31e9ff
Mela Maluenda <maluenda.mela@gmail.com>
2023-10-30 22:53:32 +0100

    Add the new Characters Provider component as a Wrapper in main

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import mainTheme from "./styles/mainTheme";
 import GlobalStyle from "./styles/GlobalStyles";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import CharactersProviderWrapper from "./features/characters/store/CharactersProviderWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <CharactersProviderWrapper>
+      <ThemeProvider theme={mainTheme}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </CharactersProviderWrapper>
   </React.StrictMode>,
 );
