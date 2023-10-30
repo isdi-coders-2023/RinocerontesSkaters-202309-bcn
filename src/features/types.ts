825b70105c3be5e41ea91c6ac8a0fffff1a941b7
Mela Maluenda <maluenda.mela@gmail.com>
2023-10-30 20:24:12 +0100

    Add character types

---
+++ src/features/types.ts
@@ -0,0 +1,16 @@
+interface CharacterStructure {
+  id: number;
+  hash: string;
+  name: string;
+  birthday: string;
+  occupation: string[];
+  img: string;
+  status: string;
+  appearance: number[];
+  firstAppearance: string;
+  lastAppearance: string;
+  nickname: string;
+  portrayed: string;
+}
+
+export default CharacterStructure;
