d4cd14e9c2f996d87bf6ccf3bb7b1f6ede39b320
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-31 14:10:11 +0100

    Add styles to home page

---
+++ src/pages/HomePage/HomePageStyled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledHomePage = styled.div`
+  background-color: ${({ theme }) => theme.colors.background};
+
+  .character-title {
+    text-align: center;
+    font-size: ${({ theme }) => theme.typography.title_size};
+    padding: 20px 0 25px;
+  }
+`;
+
+export default StyledHomePage;
