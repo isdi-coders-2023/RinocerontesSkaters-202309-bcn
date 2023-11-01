7db86750ecb6db5d0c65dd3fe3035a565a537029
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-01 01:00:17 +0100

    Modify Header test component

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Given a Header component", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
+  });
   describe("When the project's App.tsx file renders its content", () => {
     test("Then it should show in the HTML file", () => {
-      render(<Header />);
-
       const headerElement = document.querySelector("header");
       const imageElement = screen.getByAltText("Logo of Better call Lau");
 
