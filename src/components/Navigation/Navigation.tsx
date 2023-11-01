7a050d390cf9e34979362e77b9af6d66f8bcafc1
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-01 19:31:38 +0100

    Modify html elements

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -6,29 +6,38 @@ const Navigation = (): React.ReactElement => {
 
   return (
     <NavigationStyled>
-      <NavLink
-        className={`icon ${pathname === "/home" ? "icon__active" : ""}`}
-        to={"/home"}
-      >
-        <img
-          src="/images/vector-home.svg"
-          alt="Home icon"
-          width="48"
-          height="48"
-        ></img>
-      </NavLink>
-
-      <NavLink
-        className={`icon ${pathname === "" ? "icon__active" : ""}`}
-        to={"/home"}
-      >
-        <img
-          src="/images/vector-add.svg"
-          alt="Create icon"
-          width="48"
-          height="48"
-        ></img>
-      </NavLink>
+      <ul className="container-list">
+        <li className="container-list__icon">
+          <NavLink
+            className={`icon ${
+              pathname === "/home" ? "container-list__active" : ""
+            }`}
+            to={"/home"}
+          >
+            <img
+              src="/images/vector-home.svg"
+              alt="Home icon"
+              width="48"
+              height="48"
+            ></img>
+          </NavLink>
+        </li>
+        <li className="container-list__icon">
+          <NavLink
+            className={`icon ${
+              pathname === "" ? "container-list__active" : ""
+            }`}
+            to={""}
+          >
+            <img
+              src="/images/vector-add.svg"
+              alt="Create icon"
+              width="48"
+              height="48"
+            ></img>
+          </NavLink>
+        </li>
+      </ul>
     </NavigationStyled>
   );
 };
