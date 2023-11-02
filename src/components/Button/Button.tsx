9e0696b3d789c0e02caafcdd5c0fc8700f33324f
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-02 17:49:03 +0100

    Add possibility to modify classes in button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,19 +2,24 @@ import { PropsWithChildren } from "react";
 import ButtonStyled from "./ButtonStyled";
 
 interface ButtonProps extends PropsWithChildren {
+  type: "button" | "submit";
+  classModifier: string;
   onClick: () => void;
   disabled: boolean;
   text: string;
 }
 
 const Button = ({
+  type,
+  classModifier,
   onClick: actionOnClick,
   disabled,
   text,
 }: ButtonProps): React.ReactElement => {
   return (
     <ButtonStyled
-      className="button"
+      className={`button ${classModifier}`}
+      type={type}
       onClick={actionOnClick}
       disabled={disabled}
     >
