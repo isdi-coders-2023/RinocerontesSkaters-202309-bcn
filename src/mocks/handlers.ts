7454b720422bd2533e88c9b4aa5b274b496a4340
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-02 21:18:05 +0100

    Fix handlers file

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,5 @@
 import { http, HttpResponse } from "msw";
 
-const allPosts = new Map();
-
 export const handlers = [
   http.get("https://better-call-lau.onrender.com/characters/", () => {
     return HttpResponse.json([
@@ -41,36 +39,5 @@ export const handlers = [
         portrayed: "Jonathan Banks",
       },
     ]);
-  }),
-
-  http.post("/posts", async ({ request }) => {
-    const newPost = await request.json();
-    allPosts.set(newPost.id, newPost);
-
-    // Don't forget to declare a semantic "201 Created"
-    // response and send back the newly created post!
-
-    return HttpResponse.json(newPost, { status: 201 });
-  }),
-
-  http.delete("/posts/:id", ({ params }) => {
-    // All request path params are provided in the "params"
-    // argument of the response resolver.
-    const { id } = params;
-
-    // Let's attempt to grab the post by its ID.
-    const deletedPost = allPosts.get(id);
-
-    // Respond with a "404 Not Found" response if the given
-    // post ID does not exist.
-    if (!deletedPost) {
-      return new HttpResponse(null, { status: 404 });
-    }
-
-    // Delete the post from the "allPosts" map.
-    allPosts.delete(id);
-
-    // Respond with a "200 OK" response and the deleted post.
-    return HttpResponse.json(deletedPost);
   }),
 ];
