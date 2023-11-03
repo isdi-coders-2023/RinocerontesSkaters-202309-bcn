1ea9ff58922ddac3a9b066424d6ec3eab0d54185
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-03 16:00:59 +0100

    Modify Navigation test

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -7,6 +7,8 @@ import mainTheme from "../../styles/mainTheme";
 describe("Given a Navigation component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an image with an alternative text that says Home icon", () => {
+      const expectedAltText = "Home icon";
+
       render(
         <ThemeProvider theme={mainTheme}>
           <MemoryRouter>
@@ -15,30 +17,26 @@ describe("Given a Navigation component", () => {
         </ThemeProvider>,
       );
 
-      const expectedText = "Home icon";
-
-      const navigationAltText = screen.getByAltText(expectedText);
+      const navigationAltText = screen.getByAltText(expectedAltText);
 
       expect(navigationAltText).toBeInTheDocument();
     });
   });
 
-  describe("When it is rendered", () => {
-    test("Then it should show an image with an alternative text that says Create icon", () => {
-      render(
-        <ThemeProvider theme={mainTheme}>
-          <MemoryRouter>
-            <Navigation />
-          </MemoryRouter>
-        </ThemeProvider>,
-      );
+  test("Then it should show an image with an alternative text: Create icon", () => {
+    const expectedAltText = "Create icon";
 
-      const expectedText = "Create icon";
+    render(
+      <ThemeProvider theme={mainTheme}>
+        <MemoryRouter>
+          <Navigation />
+        </MemoryRouter>
+      </ThemeProvider>,
+    );
 
-      const navigationAltText = screen.getByAltText(expectedText);
+    const navigationAltText = screen.getByAltText(expectedAltText);
 
-      expect(navigationAltText).toBeInTheDocument();
-    });
+    expect(navigationAltText).toBeInTheDocument();
   });
 
   describe("When it is rendered andwe are at path `/home`", () => {
