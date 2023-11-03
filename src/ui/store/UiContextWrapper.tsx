8058567eca7eaa618d187772a70c143f7af61941
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-03 22:25:32 +0100

    Fix code smell in uiContextWrapper

--- src/ui/store/UiContextWrapper.tsx
+++ src/ui/store/UiContextWrapper.tsx
@@ -3,7 +3,9 @@ import UiContext from "./UiContext";
 
 const UiContextWrapper = ({ children }: PropsWithChildren) => {
   const [isLoading, setIsLoading] = useState(false);
+
   const [isError, setIsError] = useState(false);
+
   const contextValue = useMemo(
     () => ({ isLoading, setIsLoading, isError, setIsError }),
     [isLoading, setIsLoading, isError, setIsError],
