da0e3b6fb5b31b0504cd6268bddf226c7f97fd34
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-03 13:25:57 +0100

    Add characterMocks

---
+++ src/mocks/characterMock.ts
@@ -0,0 +1,20 @@
+import CharacterStructure from "../features/characters/types";
+
+export const characterMock: CharacterStructure = {
+  id: 1,
+  hash: "6367c33e64f544038d8d9582",
+  name: "Jimmy McGill",
+  birthday: "November 12, 1960",
+  occupation: [
+    "Mailroom clerk at Hamlin, Hamlin & McGill",
+    "Public Defense Attorney",
+  ],
+  image:
+    "https://cdn.discordapp.com/attachments/1162111119914188901/1169626363280498718/BCS_S6_Portrait_Jimmy.webp?ex=6556166f&is=6543a16f&hm=0318acdbe7203102162223d4c331645a2b4c27c9b1a6baf5ac8a420e172f721c&",
+  status: "Alive",
+  appearance: [1, 2, 3, 4, 5, 6],
+  firstAppearance: "Uno",
+  lastAppearance: "Saul Gone",
+  nickname: "Saul Goodman",
+  portrayed: "Bob Odenkirk",
+};
